a6d13410abf03862c1eed36968708f4a84e47b57
Harvy <harvyliu2009@gmail.com>
2025-09-22 09:49:38 +0800

    Added confirmation question to vimrc,sh

---
+++ vimrc.sh
@@ -0,0 +1,12 @@
+echo "NOTE: THIS SCRIPT IS ONLY FOR DEBIAN BASED LINUX OS's or WSL, if you wish to have others, i suggest you ask ChatGPT"
+
+read -p "Continue? [y/n] " answer
+if [[ $answer == [Yy] ]]; then
+  echo "Starting to install the followings"
+  # continue with the script
+else
+  echo "Process has been stopped"
+  exit
+  # Stops Process
+fi
+
