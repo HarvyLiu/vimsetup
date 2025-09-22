8c276eb51d7581902d89ccdbaf4c8f0b24b4a916
Harvy <harvyliu2009@gmail.com>
2025-09-22 12:55:43 +0800

    Finished Vim version. Going to start NeoVim Version

--- vimrc.sh
+++ vimrc.sh
@@ -1,9 +1,15 @@
-echo "NOTE: THIS SCRIPT IS ONLY FOR DEBIAN BASED LINUX OS or WSL, if you wish to have others, i suggest you ask ChatGPT"
+echo "NOTE: THIS SCRIPT IS ONLY FOR DEBIAN BASED LINUX OS or WSL or VMs, if you wish to have others, i suggest you ask ChatGPT"
 
 read -p "Continue? [y/n] " answer
 if [[ $answer == [Yy]]]; then
   echo "Starting to install the followings"
   # continue with the script
+  cd ~
+  cp .vimrc .vimrc.backup
+  echo 'Your original setup has been backuped. .vimrc >> .vimrc.backup'
+  cat vimrc.txt > .vimrc
+  echo 'Process is done! Now go to your Vim and Run :PlugInstall, ignore the errors, after you finish installing, quit and return to vim, the errors will be gone by the point. Also please do :CocInstall <theNameOfTheLanguagePackage>, if you do not know what the package name is, just search on Google.'
+  echo 'THANK YOU FOR USING MY FIRST PIECE OF BASH CODE, BY FAR MY BEST "PROJECT". cya in later projects that might or might not be a thing ~ HarvyLiu'
 else
   echo "Process has been stopped"
   exit
