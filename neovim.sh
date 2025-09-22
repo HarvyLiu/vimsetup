afe67fd78ae37a5011b09834f944a9a90dc926e3
Harvy <harvyliu2009@gmail.com>
2025-09-22 14:02:48 +0800

    Fixed some mistakes i made.(I added some code that i forgot to add

--- neovim.sh
+++ neovim.sh
@@ -7,6 +7,14 @@ if [[ $answer == [Yy]]]; then
   cd ~/.config/nvim/
   cp ~/.config/nvim/init.vim ~/.config/nvim/init.vim.backup
   echo 'Your original setup has been backuped. init.vim >> init.vim.backup'
+  sudo apt update
+  sudo apt install nodejs npm -y
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+  source ~/.bashrc
+  nvm install --lts
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  sudo apt-get install fonts-powerline
   cat initvim.txt > init.vim
   echo 'Process is done! Now go to your Neovim and Run :PlugInstall, ignore the errors, after you finish installing, quit and return to vim, the errors will be gone by the point. Also please do :CocInstall <theNameOfTheLanguagePackage>, if you do not know what the package name is, just search on Google.'
   echo 'THANK YOU FOR USING MY FIRST PIECE OF BASH CODE, BY FAR MY BEST "PROJECT". cya in later projects that might or might not be a thing ~ HarvyLiu'
--- vimrc.sh
+++ vimrc.sh
@@ -7,6 +7,14 @@ if [[ $answer == [Yy]]]; then
   cd ~
   cp .vimrc .vimrc.backup
   echo 'Your original setup has been backuped. .vimrc >> .vimrc.backup'
+  sudo apt update
+  sudo apt install nodejs npm -y
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+  source ~/.bashrc
+  nvm install --lts
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  sudo apt-get install fonts-powerline
   cat vimrc.txt > .vimrc
   echo 'Process is done! Now go to your Vim and Run :PlugInstall, ignore the errors, after you finish installing, quit and return to vim, the errors will be gone by the point. Also please do :CocInstall <theNameOfTheLanguagePackage>, if you do not know what the package name is, just search on Google.'
   echo 'THANK YOU FOR USING MY FIRST PIECE OF BASH CODE, BY FAR MY BEST "PROJECT". cya in later projects that might or might not be a thing ~ HarvyLiu'
