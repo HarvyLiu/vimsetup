21c13294cce517b006763e23e5c76900cac50ff2
Harvy <harvyliu2009@gmail.com>
2025-09-22 09:58:31 +0800

    Added typing yes could also mean y

--- vimrc.sh
+++ vimrc.sh
@@ -1,7 +1,7 @@
 echo "NOTE: THIS SCRIPT IS ONLY FOR DEBIAN BASED LINUX OS or WSL, if you wish to have others, i suggest you ask ChatGPT"
 
 read -p "Continue? [y/n] " answer
-if [[ $answer == [Yy] ]]; then
+if [[ $answer == [Yy]|[Yy][Ee][Ss]]]; then
   echo "Starting to install the followings"
   # continue with the script
 else
